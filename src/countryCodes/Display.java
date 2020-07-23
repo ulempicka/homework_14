bf6bcadf43ede4e0f5f4cb499a6a49faeedc1203
ulempicka <urszula.lempicka@gmail.com>
2020-07-23 15:24:44 +0200

    Commit 5 - updated 2 task according to comments, updated Display

--- src/countryCodes/Display.java
+++ src/countryCodes/Display.java
@@ -4,15 +4,12 @@ import java.util.Map;
 
 public class Display {
     public static void showStats(String countryCode, Map<String, Country> countries) {
-        for (String key : countries.keySet()) {
-            if (key.equals(countryCode)) {
-                Country country = countries.get(key);
-                System.out.println(country.getName() + " (" + country.getCode() + ") ma " + country.getPopulation() + " ludności.");
-            }
-            else if(!countries.containsKey(countryCode)){
-                System.out.println("Niepoprawny kod kraju!");
-                break;
-            }
+
+        if(!countries.containsKey(countryCode)){
+            System.out.println("Niepoprawny kod kraju!");
+            return;
         }
+        Country country = countries.get(countryCode);
+        System.out.println(country.getName() + " (" + country.getCode() + ") ma " + country.getPopulation() + " ludności.");
     }
 }
